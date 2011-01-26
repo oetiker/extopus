b27a41bd0f249c0214634411fc3142ca93d12eee
Tobias Oetiker <tobi@oetiker.ch>
2011-01-26 17:58:55 +0100

    made build list work on fresch ubuntu box

--- software/setup/build_dependencies.sh
+++ software/setup/build_dependencies.sh
@@ -17,21 +17,25 @@ export PREFIX=$1
 
 . `dirname $0`/module_builder.inc
 
-if prepare ftp://ftp.mozilla.org/pub/mozilla.org/js/ js-1.7.0.tar.gz; then
+if prepare ftp://ftp.mozilla.org/pub/mozilla.org/js js-1.7.0.tar.gz; then
 cd src
+env CFLAGS="-DJS_C_STRINGS_ARE_UTF8" make JS_DIST=$PREFIX -f Makefile.ref
 env CFLAGS="-DJS_C_STRINGS_ARE_UTF8" make JS_DIST=$PREFIX -f Makefile.ref export
+touch $WORKDIR/js-1.7.0.tar.gz.ok
 fi
 
-if prepare http://sourceforge.net/projects/boost/files/boost/1.45.0/ boost_1_45_0.tar.bz2; then
+if prepare http://sourceforge.net/projects/boost/files/boost/1.45.0 boost_1_45_0.tar.bz2; then
   ./bootstrap.sh
   ./bjam --prefix=$PREFIX address-model=64 variant=release --with-filesystem --with-thread --with-date_time --layout=versioned --with-program_options link=shared runtime-link=shared threading=multi install
+  touch $WORKDIR/boost_1_45_0.tar.bz2.ok
 fi
 
-if prepare http://prdownloads.sourceforge.net/scons/ scons-2.0.1.tar.gz; then
+if prepare http://prdownloads.sourceforge.net/scons scons-2.0.1.tar.gz; then
   python setup.py install --prefix=$PREFIX
+  touch $WORKDIR/scons-2.0.1.tar.gz.ok
 fi
 
-if prepare http://downloads.mongodb.org/src/ mongodb-src-r1.6.5.tar.gz ; then
+if prepare http://downloads.mongodb.org/src mongodb-src-r1.6.5.tar.gz ; then
   patch -p0 <<PATCH_END
 --- /home/oetiker/.jmacs_backup/SConstruct~     2011-01-20 17:33:16.000000000 +0100
 +++ SConstruct  2011-01-20 17:33:21.000000000 +0100
@@ -46,13 +50,19 @@ if prepare http://downloads.mongodb.org/src/ mongodb-src-r1.6.5.tar.gz ; then
       nix = True
 
 PATCH_END
-  scons --extrapathdyn=$PREFIX --prefix=$PREFIX --boost-version=1_45 --boost-compiler=gcc43 --cpppath=$PREFIX/include/boost-1_45 --sharedclient install
+  GCC=`gcc --version | perl -n -e 'm/gcc.+?(\d)\.(\d)/ && print "gcc$1$2"'`
+  scons --extrapathdyn=$PREFIX --prefix=$PREFIX --boost-version=1_45 --boost-compiler=$GCC --cpppath=$PREFIX/include/boost-1_45 --sharedclient install
+  touch $WORKDIR/mongodb-src-r1.6.5.tar.gz.ok
 fi
 
 perlmodule ExtUtils::CBuilder
 perlmodule ExtUtils::ParseXS
 perlmodule ExtUtils::Manifest
 perlmodule Test::Harness
+perlmodule Perl::OSType
+perlmodule CPAN::Meta::YAML
+perlmodule version
+perlmodule Module::Metadata
 perlmodule Module::Build
 perlmodule Class::Method::Modifiers
 perlmodule Devel::PPPort
