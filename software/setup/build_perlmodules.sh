fae04bc4cc71b89bca059af532db33c363b3103b
Tobias Oetiker <tobi@oetiker.ch>
2011-04-25 20:06:13 +0200

    fixed nameing

--- software/setup/build_perlmodules.sh
+++ software/setup/build_perlmodules.sh
@@ -9,7 +9,6 @@ export PERL_CPANM_OPT="--notest --local-lib $install"
 export PATH=$install/bin:$PATH
 export PERL5LIB=$install/lib/perl5
 
-#./build_mongodb.sh $install
 [ -e $install/bin/cpanm ] || wget --no-check-certificate -O $install/bin/cpanm cpanmin.us && chmod 755 $install/bin/cpanm
 
 cpanm   Mojolicious \
@@ -20,17 +19,3 @@ cpanm   Mojolicious \
         Devel::NYTProf \
         JSON::XS \
         Mojo::JSON::Any
-#cpanm	Mouse
-#cpanm   Any::Moose
-#cpanm   MongoDB
-#cpanm   Tie::IxHash
-#cpanm   AnyEvent::CouchDB
-#cpanm   Store::CouchDB
-#cpanm   CouchDB::Client
-#cpanm   SIAM
-#if [ -d /usr/pack/postgresql-8.4.3-za ]; then
-#  export POSTGRES_LIB=/usr/pack/postgresql-8.4.3-za/amd64-linux-ubuntu8.04/lib \
-#  export POSTGRES_INCLUDE=/usr/pack/postgresql-8.4.3-za/amd64-linux-ubuntu8.04/include
-#fi
-#cpanm   DBD::Pg
-#cpanm CHI
