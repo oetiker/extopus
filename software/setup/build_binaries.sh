a0eac01e7a012d7ab72d25b5691d96016436a42d
Tobias Oetiker <tobi@oetiker.ch>
2011-04-25 21:30:22 +0200

    du not build db modules

--- software/setup/build_binaries.sh
+++ software/setup/build_binaries.sh
@@ -35,6 +35,7 @@ fi
 if prepare http://search.cpan.org/CPAN/authors/id/R/RJ/RJBS perl-5.12.3.tar.gz; then
    make clean || true
    ./Configure -de \
+        -Ui_db \
         -Dprefix=$PREFIX \
         -Dperlpath="$PREFIX/bin/perl" \
         -Dstartperl="#!$PREFIX/bin/perl" \
