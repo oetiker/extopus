e15da5182e94f278647b2ff98c03ea7d8b5e4e74
Tobias Oetiker <tobi@oetiker.ch>
2011-04-27 16:01:15 +0200

    tested and fixed

--- build_dbd_oracle.sh
+++ build_dbd_oracle.sh
@@ -2,9 +2,11 @@
 
 . `dirname $0`/sdbs.inc
 
-if [ ! -d "$ORACLE_HOME/libnnz11.so" ]; then
+if [ ! -e "$ORACLE_HOME/libnnz11.so" ]; then
    echo "Make sure to set the ORACLE_HOME variable pointing to the directory where you unpacked instantclient and sdk"   
    exit 1;
 fi
+export LD_LIBRARY_PATH=$ORACLE_HOME
 
+perlmodule DBI
 perlmodule DBD::Oracle
