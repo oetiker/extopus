8ec5dc9460f750fc9e800383b6590f4daba773b4
Tobias Oetiker <tobi@oetiker.ch>
2011-04-27 15:46:29 +0200

    only build perl here

--- build_perl-5.12.3.sh
+++ build_perl-5.12.3.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 . `dirname $0`/sdbs.inc
 
-simplebuild ftp://xmlsoft.org/libxml2/ libxml2-2.7.8.tar.gz --without-python
-
-if prepare http://download.oracle.com/berkeley-db/ db-4.8.30.tar.gz ; then
-    cd build_unix
-    ../dist/configure \
-        "--prefix=${PREFIX}"
-    make
-    make install
-    touch $WORKDIR/db-4.8.30.tar.gz.ok
-fi
-
 if prepare http://search.cpan.org/CPAN/authors/id/R/RJ/RJBS perl-5.12.3.tar.gz; then
    make clean || true
    ./Configure -de \
