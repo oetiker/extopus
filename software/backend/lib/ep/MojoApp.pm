8e02fb38e4f211297bb3df8b9615d842f9598bf4
Tobias Oetiker <tobi@oetiker.ch>
2011-05-11 16:54:29 +0200

    sample proxy service including dom editing implemented

--- software/backend/lib/ep/MojoApp.pm
+++ software/backend/lib/ep/MojoApp.pm
@@ -5,6 +5,7 @@ use warnings;
 # load the two modules to have perl check them
 use MojoX::Dispatcher::Qooxdoo::Jsonrpc;
 use Mojolicious::Plugin::QooxdooJsonrpc;
+use Mojo::URL;
 
 use ep::JsonRpcService;
 use ep::Config;
@@ -27,20 +28,54 @@ sub startup {
     $self->secret($gcfg->{mojo_secret});
     if ($self->app->mode ne 'development'){
         $self->log->path($gcfg->{log_file});
-    }
-    if ($gcfg->{log_level}){    
-        $self->log->level($gcfg->{log_level});
+        if ($gcfg->{log_level}){    
+            $self->log->level($gcfg->{log_level});
+        }
     }
     my $routes = $self->routes;
 
     # run /setUser/oetiker to launch the application for a particular user
-    $routes->route('/setUser/(:user)')->to(
-        cb => sub {
-            my $self = shift;
-            $self->session(epUser =>  $self->stash('user'));
-            $self->redirect_to('/');
-        }          
-    );
+    $routes->get('/setUser/(:user)' => sub {
+        my $self = shift;
+        $self->session(epUser =>  $self->stash('user'));
+        $self->redirect_to('/');
+    });
+
+    $routes->get('/proxy/(:px_hash)/(*px_target)' => sub {
+        my $ctrl = shift;
+        my $hash =  $ctrl->stash('px_hash');
+        my $target =  $ctrl->stash('px_target');
+        my $prefix = 'http://tobi.oetiker.ch/';
+        my $url = Mojo::URL->new($prefix.$target);
+        $url->query($ctrl->req->params);
+        $ctrl->render_later;
+        $ctrl->ua->get($url => sub {
+            my ($self, $tx) = @_;     
+            if (my $res=$tx->success) {                
+                if ($res->headers->content_type =~ m'text/html'i){
+                    my $dom = $res->dom;
+                    $dom->find('img[src]')->each( sub { 
+                        my $attrs = shift->attrs;
+                        my $old_href = $attrs->{href}; 
+                        $attrs->{src} = 'http://www.google.ch/logos/2011/graham11-hp-start.png';
+                    });         
+                    $res->body($dom->to_xml);
+                }
+                $ctrl->tx->res($res);
+                $ctrl->rendered;
+            }
+            else {
+                my ($msg,$error) = $tx->error;
+                $ctrl->tx->res->headers->add('X-Remote-Status',$error.': '.$msg);   
+                $ctrl->render(
+                    status => 500,
+                    text => 'Failed to fetch data from backend'
+                );
+            }
+        });
+    });
+
+
 
     my $inventory = ep::Inventory->new(cfg=>$self->cfg,log=>$self->app->log);
 
