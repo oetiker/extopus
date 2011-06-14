0ba289d5b5965b5908eb63057ec101ada665d8d3
Tobias Oetiker <tobi@oetiker.ch>
2011-06-14 08:38:11 +0200

    have more cablecomly colors

--- software/frontend/source/class/ep/theme/Color.js
+++ software/frontend/source/class/ep/theme/Color.js
@@ -8,12 +8,18 @@
 
 ************************************************************************ */
 
+var lightBlue = "#6eb7e4";
+var darkBlue = "#0066a8";
 qx.Theme.define("ep.theme.Color", {
     extend : qx.theme.simple.Color,
 
     colors : {
-        "table-pane"                : "#f8f8f8",
-        "table-row-background-even" : "#F3F3F3",
-        "table-row-background-odd"  : "#FFF"
+        "table-row-background-even" : "#f3f3f3",
+        "table-row-background-odd"  : "#fff",
+        "dark-blue" : darkBlue,
+        "table-row-background-selected" : lightBlue,
+        "background-selected" : lightBlue,
+        "border-main" : lightBlue,
+        "tabview-unselected" : darkBlue        
     }
 });
