b44464437e3cd7cf84926a0b70a6127a634b4845
Tobias Oetiker <tobi@oetiker.ch>
2011-06-17 21:48:08 +0200

    if no search expression is defined, do not bother running a query

--- software/backend/lib/ep/Cache.pm
+++ software/backend/lib/ep/Cache.pm
@@ -202,8 +202,8 @@ how many nodes match the given expression
 
 sub getNodeCount {
     my $self = shift;
-    my $expression = shift;
-    my $dbh = $self->dbh;
+    my $expression = shift or return 0;
+    my $dbh = $self->dbh;    
     return (($dbh->selectrow_array("SELECT count(docid) FROM node WHERE data MATCH ?",{},$self->encodeUtf8->encode($expression)))[0]);
 }
 
@@ -216,7 +216,7 @@ Return nodes matching the given search term
 
 sub getNodes {
     my $self = shift;
-    my $expression = shift;    
+    my $expression = shift or return [];    
     my $limit = shift || 100;
     my $offset = shift || 0;
     my $dbh = $self->dbh;
