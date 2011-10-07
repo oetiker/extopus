b94ae163630327e3ed99dbde978c2a566b92725d
Tobias Oetiker <tobi@oetiker.ch>
2011-10-07 16:22:33 +0200

    initial autoconf support

--- software/setup/build-perl-modules.sh
+++ software/setup/build-perl-modules.sh
@@ -2,15 +2,10 @@
 
 . `dirname $0`/sdbs.inc
 
-if python -V 2>&1 | egrep -q '2.[5-9]'; then
-:
-else
-   simplebuild http://www.python.org/ftp/python/2.7.1 Python-2.7.1.tgz
-fi
-
 for module in \
     Mojolicious \
     MojoX::Dispatcher::Qooxdoo::Jsonrpc \
+    Mojo::Server::FastCGI \
     Config::Grammar \
     SIAM \
     DBD::SQLite \
