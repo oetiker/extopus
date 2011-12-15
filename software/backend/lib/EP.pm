20f7aa1b784cce18adc4958dd8c3dc7aacd40346
Tobias Oetiker <tobi@oetiker.ch>
2011-12-15 14:26:01 +0100

    improve logic for figuring url prefix

--- software/backend/lib/EP.pm
+++ software/backend/lib/EP.pm
@@ -86,9 +86,12 @@ sub startup {
             $self->log->level($gcfg->{log_level});
         }
     }
+    
     $self->hook( before_dispatch => sub {
         my $self = shift;
         my $uri = $self->req->env->{SCRIPT_URI};
+        my $path_info = $self->req->env->{PATH_INFO};
+        $uri =~ s|/?${path_info}$|/| if $path_info and $uri;
         $self->req->url->base(Mojo::URL->new($uri)) if $uri;
     });
     
