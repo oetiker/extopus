e99a4d0af7610a243b624493aacbc979345134c3
Tobias Oetiker <tobi@oetiker.ch>
2012-02-10 14:41:13 +0100

    rearange icon structure

---
+++ software/frontend/source/class/ep/ui/ShortNote.js
@@ -0,0 +1,66 @@
+/* ************************************************************************
+   Copyright: 2011 OETIKER+PARTNER AG
+   License:   GPLv3 or later
+   Authors:   Tobi Oetiker <tobi@oetiker.ch>
+   Utf8Check: äöü
+************************************************************************ */
+
+/*
+#asset(qx/icon/${qx.icontheme}/16/status/dialog-information.png)
+*/
+
+/**
+ * A popup at the top edge of the display, hiding itself soon after appearing.
+ * will re-appear when its label is set.
+ */
+qx.Class.define("ep.ui.ShortNote", {
+    extend : qx.ui.container.Composite,
+    type: 'singleton',
+
+    /**
+     * wrap the loading Box around the given widget
+     *
+     * @param widget {Widget} widget to wrap
+     */
+    construct : function() {
+        this.base(arguments,new qx.ui.layout.HBox().set({
+            alignX: 'center'
+        }));
+        this.set({
+            zIndex: -10
+        });
+        var note = this.__note = new qx.ui.basic.Atom('',"icon/16/status/dialog-information.png");
+        note.set({
+            textColor : "tooltip-text",
+            backgroundColor : "tooltip",
+            decorator: "main-dark",
+            padding : [ 1, 3, 2, 3 ],
+        });
+
+        this.add(note);
+        this.addListener('click',function(){
+            this.hide();
+        },this);
+        this.getApplicationRoot().add(this,{top:0,left:0,right:0});
+    },
+
+    members: {
+        __timer: null,
+        __note: null,
+        /**
+         * popup the info and show its content
+         *
+         * @param text {String} text to show
+         */
+        setNote: function(text){
+            this.__note.setLabel(String(text));
+            if (this.__timer){
+                this.__timer.stop();
+            }
+            this.show();
+            this.__timer = qx.event.Timer.once(function(){
+                this.hide();
+            },this,1500);
+        }
+    }
+});
