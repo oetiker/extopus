af04df88e35bd7d499e519bcd1431d6ec4999791
Tobias Oetiker <tobi@oetiker.ch>
2013-12-09 10:59:43 +0100

    use hmac_sha1_sum instead of hmac_md5_sum

--- software/backend/lib/EP/Visualizer/base.pm
+++ software/backend/lib/EP/Visualizer/base.pm
@@ -19,7 +19,7 @@ The base class for extopus visualizers
 =cut
 
 use Mojo::Base -base;
-use Mojo::Util qw(hmac_md5_sum);
+use Mojo::Util qw(hmac_sha1_sum);
 
 =head2 cfg
 
@@ -155,7 +155,7 @@ Returns a hash for authenticating access to the ref
 sub calcHash {   ## no critic (RequireArgUnpacking)
     my $self = shift;
     # $self->log->debug('HASH '.join(',',@_));    
-    my $hash = hmac_md5_sum(join('::',@_),$self->app->secret);
+    my $hash = hmac_sha1_sum(join('::',@_),$self->app->secret);
     return $hash;
 }
 
