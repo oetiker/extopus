b740019be1572abe778c5f1210815c52a20e47e5
Tobias Oetiker <tobi@oetiker.ch>
2013-12-09 09:58:07 +0100

    tracking mojo api changes

--- software/backend/bin/ep.pl
+++ software/backend/bin/ep.pl
@@ -5,16 +5,8 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../thirdparty/lib/perl5";
 use lib "$FindBin::Bin/../lib";
-# use lib qw() # PERL5LIB
 use Mojolicious::Commands;
-use EP;
 
 our $VERSION = "0";
 
-local $ENV{MOJO_APP} = 'EP';
-
-my $cmds = Mojolicious::Commands->new();
-push @{$cmds->namespaces}, 'EP::Command';
-
-# Start commands
-$cmds->start;
+Mojolicious::Commands->start_app('EP');
