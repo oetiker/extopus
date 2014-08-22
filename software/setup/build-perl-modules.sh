8fcee023e2b094289c3fc341a3fbe1914e0d6835
Tobias Oetiker <tobi@oetiker.ch>
2014-08-22 18:11:14 +0200

    update build infrastructure

--- software/setup/build-perl-modules.sh
+++ software/setup/build-perl-modules.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-. `dirname $0`/sdbs.inc
+dir=`dirname $0`;
+. $dir/sdbs.inc
 
-for module in \
-    Mojolicious@3.54 \
-    MojoX::Dispatcher::Qooxdoo::Jsonrpc@0.88 \
-    Mojo::Server::FastCGI@0.41 \
-    Config::Grammar@1.10 \
-    SIAM@0.10 \
-    DBD::SQLite@1.42 \
-    JSON::XS@3.01 \
-    Mojo::JSON::Any@0.990104 \
-    Excel::Writer::XLSX@0.77  \
-    Spreadsheet::WriteExcel@2.40 \
-; do
+for module in `perl -ne 'chomp; print qq{$_ }' $dir/../PERL_MODULES `; do
     perlmodule $module
 done
 
-#    Devel::NYTProf 
+
         
